3d9a2b90ecc479e388b8d3e4c2b80e1ecaf470a7
tof <christof@taobits.net>
2017-10-27 21:11:50 +0200

    Completed chapter pure functions

--- src/test/kotlin/a__first_class_functions/Customer.kt
+++ src/test/kotlin/a__first_class_functions/Customer.kt
@@ -1,11 +1,12 @@
 package a__first_class_functions
 
+import b__pure_functions.Contract
+import org.amshove.kluent.`should be`
 import org.amshove.kluent.`should equal`
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
-
-import b__pure_functions.Contract
+import org.jetbrains.spek.api.dsl.on
 
 data class Customer(
         val id: Int,
@@ -18,26 +19,49 @@ data class Customer(
         var contract: Contract? = null)
 
 typealias FieldSelection<T> = (Customer) -> T
+typealias CustomerFilter = (Customer) -> Boolean
 
 fun <T> getEnabledCustomerFields(customers: List<Customer>, fieldSelection: FieldSelection<T>): List<T> =
-        customers.filter({it.enabled}).map(fieldSelection)
+        getFilteredCustomerFields(customers, {it.enabled}, fieldSelection)
+
+fun <T> getFilteredCustomerFields(customers: List<Customer>, customerFilter: CustomerFilter, fieldSelection: FieldSelection<T>): List<T> =
+        customers
+            .filter { customerFilter(it) }
+            .map(fieldSelection)
+
+fun getCustomerById(customers: List<Customer>, id: Int): Customer? = customers.firstOrNull {it.id == id}
+
+
+val allCustomers = listOf(
+        Customer(1, "BVG", "Berlin", "Germany", "Hans", "bvg.de"),
+        Customer(2, "Axel Springer", "Berlin", "Germany", "Axel", "as.de", false),
+        Customer(3, "Deutsche Bahn", "Franfurt", "Germany", "Dorn", "db.de")
+)
 
 class CustomersSpec: Spek({
-    describe("some customers") {
-        val customers = listOf(
-                Customer(1, "BVG", "Berlin", "Germany", "Hans", "bvg.de"),
-                Customer(2, "Axel Springer", "Berlin", "Germany", "Axel", "as.de", false),
-                Customer(3, "Deutsche Bahn", "Franfurt", "Germany", "Dorn", "db.de")
-        )
-
+    describe("using all customers") {
         it("getEnabledCustomerFields with function selecting name should return name of enabled customers") {
-            val result = getEnabledCustomerFields(customers, { it.name })
+            val result = getEnabledCustomerFields(allCustomers) { it.name }
             result `should equal` listOf("BVG", "Deutsche Bahn")
         }
         it("should return email with parametrized recipient using a closure to access outside value") {
             val recipient = "boss"
-            val result = getEnabledCustomerFields(customers, { "$recipient@${it.domain}" })
+            val result = getEnabledCustomerFields(allCustomers) { "$recipient@${it.domain}" }
             result `should equal` listOf("boss@bvg.de", "boss@db.de")
         }
+        on("getCustomerById with existing id") {
+            val result = getCustomerById(allCustomers, 1)
+            it("should return customer") {
+                result `should equal` allCustomers[0]
+            }
+        }
+        on("getCustomerById with non-existing id") {
+            val result = getCustomerById(allCustomers, 0)
+            it("should return customer") {
+                result `should be` null
+            }
+        }
+
     }
 })
+
--- src/test/kotlin/b__pure_functions/Contract.kt
+++ src/test/kotlin/b__pure_functions/Contract.kt
@@ -1,12 +1,15 @@
 package b__pure_functions
 
+import a__first_class_functions.Customer
+import a__first_class_functions.allCustomers
+import a__first_class_functions.getCustomerById
 import org.amshove.kluent.`should be`
 import org.amshove.kluent.`should equal`
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
 import org.jetbrains.spek.api.dsl.it
+import org.jetbrains.spek.api.dsl.on
 import java.util.*
-
 
 data class Contract(
         val beginDate: Date,
@@ -18,6 +21,16 @@ fun defaultEndDate(beginDate: Date): Date {
     cal.time = beginDate
     cal.add(Calendar.YEAR, 2)
     return cal.time
+}
+
+fun setContractForCustomer(customers: List<Customer>, customerId: Int, enable: Boolean): Contract? {
+    val contract = getCustomerById(customers, customerId)?.contract
+    return if (contract != null) {
+        contract.enabled = enable
+        contract
+    } else {
+        null
+    }
 }
 
 fun date(year: Int, month: Int, day: Int): Date {
@@ -37,5 +50,34 @@ class ContractSpec: Spek({
             contract.enabled `should be` true
         }
     }
+    describe("some contract with customer BVG") {
+        val contract = Contract(date(2017, 27, 10))
+        contract.enabled `should be` true // Enabled by default
+        getCustomerById(allCustomers, 1)!!.contract = contract
+
+        on("disable contract for customer") {
+            val result = setContractForCustomer(allCustomers,1, false)
+
+            it("contract should be disabled") {
+                contract.enabled `should be` false
+                result `should be` contract
+            }
+        }
+        on("disable contract for non-existing customer") {
+            val result = setContractForCustomer(allCustomers, -1, false)
+            it("should be ignored") {
+                result `should be` null
+            }
+        }
+        on("disable contract for customer without contract") {
+            val result = setContractForCustomer(allCustomers, 2, false)
+            it("should be ignored") {
+                result `should be` null
+                getCustomerById(allCustomers, 2)!!.contract `should be` null
+            }
+        }
+
+    }
 })
+
 
